8e740756993de0abba2999314364ac20ef1ba52c
Đinh Huỳnh Gia Bảo <giabaoctg@gmail.com>
2025-04-09 23:30:36 +0700

    add(fl): add data loader

--- data/data-loader.py
+++ data/data-loader.py
@@ -1,2 +1,28 @@
 # Haven't finished this yet
 # This file is used to load the data from the data directory
+
+'''
+This file is used to load the data from the data directory
+Remember to change the path to the data directory in the config file
+'''
+
+# Importing the necessary libraries
+
+class DataLoader:
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+
+    def load_data(self):
+        # Load the data from the data directory
+        # This function should return the data in a format that can be used by the model
+        pass
+
+    def preprocess_data(self, data):
+        # Preprocess the data
+        # This function should return the preprocessed data
+        pass
+
+    def split_data(self, data):
+        # Split the data into training and testing sets
+        # This function should return the training and testing sets
+        pass
